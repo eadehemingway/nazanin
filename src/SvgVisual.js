b71a23844e684e22574014b762e35d475f297e2e
Eade Hemingway <eadehemingway@Eades-MBP.lan>
2022-04-03 15:52:51 +0100

    eslint and initial components

---
+++ src/SvgVisual.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+
+export function SvgVisual(){
+
+    return <Container>svg</Container>;
+}
+
+const Container = styled.div`
+border: 2px solid blue;
+width: 70%;
+height: 100vh;
+
+`;
---
+++ src/TextContainer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export function TextContainer(){
+
+    return <Container>hh</Container>;
+}
+
+const Container = styled.div`
+    border: 2px solid red;
+    width: 30%;
+    height: 100vh;
+
+`;
