8ba9269b3d07830a9bce374ba7d7b3796f4b04de
Eade Hemingway <eadehemingway@Eades-MBP.lan>
2022-04-03 18:33:30 +0100

    create scrolltriggers for first two paragraphs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import "./App.css";
+import gsap from "gsap";
 import { useEffect, useState } from "react";
 import * as d3 from "d3";
 import styled from "styled-components";
@@ -11,16 +11,18 @@ function App() {
     const [accessor, setAccessor] = useState("articles");
 
     // set up some scroll thing that updates the accessor which then updates the fills...
-    // useEffect(()=> {
-    //     d3.selectAll("rect").attr("fill", (d)=>{
-    //         const color = getFill(d, accessor);
-    //         return color;
-    //     });
-    // }, [accessor]);
+    useEffect(()=> {
+        d3.selectAll("rect").attr("fill", (d)=>{
+            const color = getFill(d, accessor);
+            return color;
+        });
+    }, [accessor]);
+
+
     return (
         <PageContainer>
             <SvgVisual/>
-            <TextContainer/>
+            <TextContainer setAccessor={setAccessor}/>
         </PageContainer>
     );
 }
